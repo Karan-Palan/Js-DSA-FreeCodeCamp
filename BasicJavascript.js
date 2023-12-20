16f012a948228bd8bc02b81827bd297829b341a1
Karan-Palan <karanpalan007@gmail.com>
2023-12-20 17:52:09 +0530

    Initial commit

---
+++ BasicJavascript.js
@@ -0,0 +1,39 @@
+// 1. Comment Your JavaScript Code
+
+// abcde
+/* 
+abcde 
+*/
+
+// 2. Declare JavaScript Variables
+
+var myName ;
+
+// 3. Storing Values with the Assignment Operator
+
+// Setup
+var a;
+
+// Only change code below this line
+a = 7;
+
+//4. Assigning the Value of One Variable to Another
+
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+
+b = a;
+
+// 5. Initializing Variables with the Assignment Operator
+
+var a = 9;
+
+// 6. Declare String Variables
+
+var myFirstName = "Karan"
+var myLastName = "Palan"
+
