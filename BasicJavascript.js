06e1d06ed22798c416dbcf99ca4f818a26ec8f1d
Karan-Palan <karanpalan007@gmail.com>
2023-12-27 20:11:23 +0530

    Update

--- BasicJavascript.js
+++ BasicJavascript.js
@@ -151,3 +151,16 @@ let c = 4.6;
 a *=  5;
 b *= 3 ;
 c *= 10;
+
+// Compound Assignment With Augmented Division
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /=  12;
+b /=  4;
+c /=  11;
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."
